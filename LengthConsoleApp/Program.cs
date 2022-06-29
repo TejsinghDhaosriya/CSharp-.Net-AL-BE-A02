b8f063f694fc9da6325213d4ab8200a9b63e86d9
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-06-29 22:19:42 +0530

    length logic

--- LengthConsoleApp/Program.cs
+++ LengthConsoleApp/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LengthClassLibrary;
 
 namespace LengthConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Write("Enter string whose length is to be calculated: ");
+            var data = Console.ReadLine();
+
+            var lh = new LengthHelper();
+            var length = lh.findLength(data);
+
+            Console.WriteLine("Length is :"+ length);
+            Console.Read();
         }
     }
 }
