7089c31ee61ab9cbf36fcde5d139d49be2abc7e4
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-06-29 22:41:52 +0530

    class file

---
+++ LengthClassLibrary/LengthClassLibrary/LengthHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthClassLibrary
+{
+    public class LengthHelper
+    {
+        public int findLength(string data)
+        {
+            return data?.Length ?? 0;
+        }
+    }
+}
