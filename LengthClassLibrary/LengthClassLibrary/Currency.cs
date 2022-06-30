92204cacd5465329c87707f64c2bad0de3343f05
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-06-30 19:23:25 +0530

    extension class

---
+++ LengthClassLibrary/LengthClassLibrary/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using LengthClassLibrary.exceptions;
+
+namespace LengthClassLibrary
+{
+    public static class Currency
+    {
+        public static string ToCurrency(this LengthHelper lengthHelper,string value)
+        {
+            if (value.Any(char.IsDigit))
+                 throw new InvalidCurrencyException(value);
+            return "$"+value;
+        }
+    }
+/*
+    Extend the string class, add a new function called ToCurrency(). 
+if string contains numbers,
+the function will prepend $ sign to and return a new string or else 
+it will throw an exception.*/
+}
---
+++ LengthClassLibrary/LengthClassLibrary/exceptions/InvalidCurrencyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthClassLibrary.exceptions
+{
+    public class InvalidCurrencyException:ApplicationException
+    {
+        public InvalidCurrencyException() { }
+
+        public InvalidCurrencyException(string name)
+            : base($"Invalid Currency Passed: {name}")
+        {
+
+        }
+    }
+}
